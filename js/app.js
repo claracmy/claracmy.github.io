25d4eb270afb1ea002622dca15e06fa257329093
claracmy <cheung.cmy@gmail.com>
2017-12-28 17:41:17 +0000

    slideshow of projects added

--- js/app.js
+++ js/app.js
@@ -1,28 +1,19 @@
 $(document).ready(function() {
-  $('#fullpage').fullpage();
+  $('#fullpage').fullpage({
+    navigation: true,
+    navigationTooltips: ['Home', 'About Me', 'Work', 'Contact'],
+    showActiveTooltip: true,
+    slidesNavigation: true,
+    responsiveSlides: true,
+    dragAndMove: 'horizontal',
+    interlockedSlides: true,
+    scrollHorizontally: true
+  });
+
   var typed = new Typed('.type', {
-    strings: ['I am a Web Developer.', 'I am a gamer.', 'I am a hiker.', 'I am a home barista.', 'I am a pharmacist.', 'I am a lifelong learner.'],
+    strings: ['I am a web developer.', 'I am a gamer.', 'I am a hiker.', 'I am a home barista.', 'I am a pharmacist.', 'I am a lifelong learner.'],
     typeSpeed: 40,
     smartBackspace: true,
     loop: true
   });
-
-  // const typed = new Typed('.type', {
-  //   strings: ['I am a web developer.', 'I am a gamer.', 'I am a hiker.'],
-  //   typeSpeed: 90
-  // });
-
-  // $('.type').typed({
-  //   strings: ['I am a web developer.', 'I am a gamer.', 'I am a hiker.'],
-  //   typeSpeed: 90
-  // });
-
 });
-
-//
-// $(function(){
-//   $('.type').typed({
-//     strings: ['I am a web developer.', 'I am a gamer.', 'I am a hiker.'],
-//     typeSpeed: 90
-//   });
-// });
